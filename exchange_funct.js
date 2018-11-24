b4f109845b9e8e984f7b12ebe0a4160748f25bdf
Alex <alex.druk@gmail.com>
2018-11-24 21:39:31 +0100

    added getMarketHist, tryed to refactor errors (without success)

--- exchange_funct.js
+++ exchange_funct.js
@@ -1,8 +1,9 @@
 const ccxt = require('ccxt');
+const moment = require('moment');
 
+const records = 200;
 module.exports = {
-    get_ins: async function (exchange, symbol, interval) {
-        const records = 200
+    get_ins: async function (exchange, symbol, interval, records) {
         try {
             //let low, high, open, close, vol, at = [];
             let ins = {
@@ -22,30 +23,7 @@ module.exports = {
             ins.volume = ohlcv.slice(-records).map(x => x[5]); // volume    
             return ins;
         } catch (e) {
-            if (e instanceof ccxt.DDoSProtection) {
-                console.log(exchange, '[DDoS Protection] ' + e.message);
-                //                continue;
-            } else if (e instanceof ccxt.RequestTimeout) {
-                console.log(exchange, '[Request Timeout] ' + e.message);
-                //                continue;
-            } else if (e instanceof ccxt.AuthenticationError) {
-                console.log(exchange, '[Authentication Error] ' + e.message);
-                //                continue;
-            } else if (e instanceof ccxt.ExchangeNotAvailable) {
-                console.log(exchange, '[Exchange Not Available] ' + e.message);
-                //                continue;
-            } else if (e instanceof ccxt.ExchangeError) {
-                console.log(exchange.id, '[Exchange Error] ' + e.message);
-                //                continue;
-            } else if (e instanceof ccxt.NetworkError) {
-                console.log(exchange.id, '[Network Error] ' + e.message);
-                //               continue;
-            } else {
-                log.red({
-                    "error": e
-                });
-                return null;
-            }
+            this.parseError(e)
         }
 
     },
@@ -61,61 +39,97 @@ module.exports = {
                 return null;
             }
         } catch (e) {
-
-            if (e instanceof ccxt.DDoSProtection || e.message.includes('ECONNRESET')) {
-                console.log('[DDoS Protection] ' + e.message);
-            } else if (e instanceof ccxt.RequestTimeout) {
-                console.log('[Request Timeout] ' + e.message);
-            } else if (e instanceof ccxt.AuthenticationError) {
-                console.log('[Authentication Error] ' + e.message);
-            } else if (e instanceof ccxt.ExchangeNotAvailable) {
-                console.log('[Exchange Not Available Error] ' + e.message);
-            } else if (e instanceof ccxt.ExchangeError) {
-                console.log('[Exchange Error] ' + e.message);
-            } else if (e instanceof ccxt.NetworkError) {
-                console.log('[Network Error] ' + e.message);
-            } else {
-                throw e;
-            }
+            this.parseError(e)
         }
     },
     get_last_price: async function (exchange, symbol) {
         if (exchange.has['fetchTicker']) {
-            let ticker = await exchange.fetchTicker(symbol);
-            //            console.log('last price:', ticker.last);
-            return ticker.last;
+            try {
+                let ticker = await exchange.fetchTicker(symbol);
+                //            console.log('last price:', ticker.last);
+                return ticker.last;
+            } catch (error) {
+                this.parseError(e)
+            }
         } else {
             console.log(exchange, 'has no method to fetch ticker.')
             return null;
         }
     },
     get_orderBook: async function (exchange, symbol) {
-        records = 20;
+        let toshow = 20;
         let orderBook = {
             bids: [],
             asks: []
         };
         try {
             let book = await exchange.fetchOrderBook(symbol);
-            orderBook.bids = book.bids.slice(0, records); //first 20
-            orderBook.asks = book.asks.slice(0, records);
+            orderBook.bids = book.bids.slice(0, toshow); //first 20
+            orderBook.asks = book.asks.slice(0, toshow);
             return orderBook;
         } catch (e) {
-            if (e instanceof ccxt.DDoSProtection || e.message.includes('ECONNRESET')) {
-                console.log('[DDoS Protection] ' + e.message);
-            } else if (e instanceof ccxt.RequestTimeout) {
-                console.log('[Request Timeout] ' + e.message);
-            } else if (e instanceof ccxt.AuthenticationError) {
-                console.log('[Authentication Error] ' + e.message);
-            } else if (e instanceof ccxt.ExchangeNotAvailable) {
-                console.log('[Exchange Not Available Error] ' + e.message);
-            } else if (e instanceof ccxt.ExchangeError) {
-                console.log('[Exchange Error] ' + e.message);
-            } else if (e instanceof ccxt.NetworkError) {
-                console.log('[Network Error] ' + e.message);
-            } else {
-                throw e;
-            }
+            this.parseError(e)
         }
-    }
+    },
+    getMarketHist: async function (exchange, symbol) {
+        let ha = [];
+        let toshow = 10;
+        let timestamp_aday_ago = moment(new Date).subtract(1, 'days').valueOf();
+        try {
+            let hist_arr = await exchange.fetchTrades(symbol, timestamp_aday_ago);
+            hist_arr = hist_arr.reverse().slice(0, toshow);
+            for (let i = 0; i < hist_arr.length; i++) {
+                let hist = {
+                    time: '',
+                    type: '',
+                    side: '',
+                    price: 0,
+                    amount: '0',
+                    cost: '0'
+                };
+                hist.time = hist_arr[i].timestamp ? moment(hist_arr[i].timestamp).format('HH:mm:ss') : '';
+                hist.type = hist_arr[i].type ? hist_arr[i].type : '';
+                hist.side = hist_arr[i].side ? hist_arr[i].side : '';
+                hist.price = hist_arr[i].price ? hist_arr[i].price : 0;
+                hist.amount = hist_arr[i].amount ? hist_arr[i].amount.toFixed(8) : '0';
+                hist.cost = hist_arr[i].cost ? hist_arr[i].cost.toFixed(8) : '0';
+                ha.push(hist);
+            }
+            return ha;
+        } catch (e) {
+            this.parseError(e)
+        }
+    },
+    sendError: function (data) {
+        const message_str = JSON.stringify(data);
+        if (ws) {
+            ws.send(message_str);
+        } else {
+            console.log(message_str);
+        }
+    },
+    parseError: function (e) {
+        if (e instanceof ccxt.DDoSProtection || e.message.includes('ECONNRESET')) {
+            console.log('[DDoSProtection]' + e.message);
+            //            this.sendError(ws, 'Fatal error: [DDoS Protection] ' + e.message) + ' Try to restart.';
+        } else if (e instanceof ccxt.RequestTimeout) {
+            console.log('[Request Timeout] ' + e.message + ' Try to restart later');
+            //            this.sendError(ws, 'Error: [Request Timeout] ' + e.message);
+        } else if (e instanceof ccxt.AuthenticationError) {
+            console.log('[Authentication Error] ' + e.message);
+            //            this.sendError(ws, 'Fatal error: [Authentication Error] ' + e.message + ' Enter valid keys.');
+        } else if (e instanceof ccxt.ExchangeNotAvailable) {
+            console.log('[Exchange Not Available Error] ' + e.message + ' Try to restart later');
+            //            this.sendError(ws, 'Error: [Exchange Not Available Error] ' + e.message);
+        } else if (e instanceof ccxt.ExchangeError) {
+            console.log('[Exchange Error] ' + e.message);
+            //            this.sendError(ws, 'Fatal error: [Exchange Error] ' + e.message + ' Try to restart.');
+        } else if (e instanceof ccxt.NetworkError) {
+            console.log('[Network Error] ' + e.message);
+            //            this.sendError(ws, 'Fatal error: [Network Error] ' + e.message + ' Try to restart later');
+        } else {
+            //            this.sendError('Fatal error: ' + e.message);
+            throw e;
+        }
+    },
 }
