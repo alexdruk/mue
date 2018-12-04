d377bbaf87f07c4604bbd651843125050ad58783
Alex <alex.druk@gmail.com>
2018-12-04 15:49:28 +0100

    added depth, commented writing to file

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const ccxt = require("ccxt");
 const creds = require("./api_keys.js");
 const exchange_funct = require("./exchange_funct.js");
 const WebSocket = require("ws");
+const moment = require("moment");
 const exchangeId = "bittrex";
+const fs = require("fs");
+const filename = "./data/orders.csv";
 const exchangeClass = ccxt[exchangeId];
 const exchange = new ccxt.bittrex({
   apiKey: creds.apiKey,
@@ -18,7 +21,10 @@ let storage = {
   inibalance_as_string: "",
   tick: 0
 };
-
+let prev_last_bid;
+let prev_last_bid_vol;
+let prev_last_ask;
+let prev_last_ask_vol;
 //-----------------------------------------------------------------------------
 let symbol = "MUE/BTC"; // default
 let cycle;
@@ -27,13 +33,14 @@ let cycle;
 //startCycle();
 
 function setTicker(ws, ticker) {
+  console.log("new ticker", ticker);
   symbol = ticker;
   getData(ws);
 }
 
 function stopCycle() {
   if (cycle) {
-    //    clearInterval(cycle);
+    clearInterval(cycle);
     cycle = null;
   }
 }
@@ -56,10 +63,10 @@ function sendData(ws, data) {
   }
 }
 async function getData(ws) {
-  console.log("Running", new Date());
+  console.log("Tick " + storage.tick + " - " + symbol + " - " + new Date());
   sendData(ws, {
     exec: "info",
-    data: "Tick " + storage.tick + " - " + new Date()
+    data: "Tick " + storage.tick + " - " + symbol + " - " + new Date()
   });
   /* test error
     if (storage.tick == 2) {
@@ -83,11 +90,12 @@ async function getData(ws) {
       data: e
     });
   });
+
   let [assets_name, currency_name] = symbol.split("/");
   let last_price = await exchange_funct.get_last_price(exchange, symbol);
   //    last_price = parseFloat(last_price);
   if (last_price) {
-    console.log("Initial price:", last_price);
+    //    console.log("Initial price:", last_price);
   }
   let balanceinfo = await exchange_funct.get_balance(exchange);
   if (balanceinfo) {
@@ -124,7 +132,7 @@ async function getData(ws) {
       currency_name +
       ")";
     storage.inibalance_as_string = balance;
-    console.log("Initial balance:", storage.inibalance_as_string);
+    //    console.log("Initial balance:", storage.inibalance_as_string);
   }
 
   let ins = {
@@ -141,13 +149,13 @@ async function getData(ws) {
   //         console.log(key, moment(ins.at[key]).format("DD-MM-YYYY HH:mm"), ins.open[key], ins.high[key], ins.low[key], ins.close[key], ins.volume[key])
   //     }
   // }
-  let orderbook = await exchange_funct.get_orderBook(exchange, symbol);
+  let orderbook = await exchange_funct.get_orderBook(exchange, symbol, 20);
   sendData(ws, {
     exec: "orderbook",
     data: orderbook
   });
 
-  let marketHistory = await exchange_funct.getMarketHist(exchange, symbol); //symbol, since = undefined, limit = undefined, params = {}
+  let marketHistory = await exchange_funct.get_MarketHist(exchange, symbol, 10); //symbol, since = undefined, limit = undefined, params = {}
   // console.log('history', marketHistory);
   sendData(ws, {
     exec: "marketHistory",
@@ -165,7 +173,73 @@ async function getData(ws) {
     exec: "closedOrders",
     data: closedOrders
   });
+  /*
+  let msg;
+  let lastorderbook = await exchange_funct.get_orderBook(exchange, symbol, 1);
+  let last_bid = lastorderbook.bids[0][0];
+  let last_bid_vol = lastorderbook.bids[0][1];
+  let last_ask = lastorderbook.asks[0][0];
+  let last_ask_vol = lastorderbook.asks[0][1];
+  if (
+    (last_bid != prev_last_bid ||
+      last_ask != prev_last_ask ||
+      last_bid_vol != prev_last_bid_vol ||
+      last_ask_vol != prev_last_ask_vol) &&
+    storage.tick > 0
+  ) {
 
+    let last_order = await exchange_funct.get_MarketHist(exchange, symbol, 1);
+    msg =
+      last_order[0].time +
+      "," +
+      last_order[0].side +
+      "," +
+      last_order[0].price +
+      "," +
+      last_order[0].amount +
+      "\n";
+    console.log(msg);
+    fs.appendFile(filename, msg, function(err) {
+      if (err) throw err;
+    });
+
+    msg =
+      moment(new Date()).format("HH:mm:ss") +
+      "," +
+      last_bid +
+      "," +
+      last_bid_vol +
+      "," +
+      last_ask +
+      "," +
+      last_ask_vol +
+      "\n";
+    console.log(msg);
+    fs.appendFile(filename, msg, function(err) {
+      if (err) throw err;
+    });
+    prev_last_bid = last_bid;
+    prev_last_bid_vol = last_bid_vol;
+    prev_last_ask = last_ask;
+    prev_last_ask_vol = last_ask_vol;
+  }
+
+  let data = await exchange_funct.get_MarketHist(exchange, symbol, 1000);
+  for (let i = 0; i < data.length; i++) {
+    let msg =
+      data[i].time +
+      "," +
+      data[i].side +
+      "," +
+      data[i].price +
+      "," +
+      data[i].amount +
+      "\n";
+    fs.appendFile(filename, msg, function(err) {
+      if (err) throw err;
+    });
+  }
+*/
   storage.tick++;
 }
 //getData();
