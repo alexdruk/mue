59c00122e54d3566c856dbd8f5b3790f5aa67da1
Alex <alex.druk@gmail.com>
2018-12-04 15:48:47 +0100

    unsignificant changes

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
-const express = require('express');
-const path = require('path');
-const WebSocket = require('ws');
+const express = require("express");
+const path = require("path");
+const WebSocket = require("ws");
 const opn = require("opn");
 
 const PORT = 8080;
 const WSS_PORT = 8081;
 
-let crypto = require('./index.js');
+let crypto = require("./index.js");
 
 let app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 let wss = new WebSocket.Server({
-    port: WSS_PORT,
+  port: WSS_PORT
 });
 
-wss.on("connection", (ws) => {
-    console.log("Opened");
-    crypto.startCycle(ws);
+wss.on("connection", ws => {
+  console.log("Opened");
+  crypto.startCycle(ws);
 
-    ws.on("message", (mesage_string) => {
-        let parsedData = JSON.parse(mesage_string);
-        switch (parsedData.exec) {
-            case 'setTicker':
-                try {
-                    crypto.setTicker(ws, parsedData.ticker);
-                } catch (e) {
-                    console.log(e);
-                }
-                break;
+  ws.on("message", mesage_string => {
+    let parsedData = JSON.parse(mesage_string);
+    switch (parsedData.exec) {
+      case "setTicker":
+        try {
+          crypto.setTicker(ws, parsedData.data);
+        } catch (e) {
+          console.log(e);
         }
-    });
+        break;
+    }
+  });
 
-    ws.on("close", () => {
-        crypto.stopCycle(ws);
-        console.log(("If you closed the site, please navigate to http://localhost:" + PORT + " to access it again, or press ctrl+c to stop the program").blue);
-    });
+  ws.on("close", () => {
+    crypto.stopCycle(ws);
+    console.log(
+      (
+        "If you closed the site, please navigate to http://localhost:" +
+        PORT +
+        " to access it again, or press ctrl+c to stop the program"
+      ).blue
+    );
+  });
 
-    ws.on('error', () => {});
+  ws.on("error", () => {});
 });
 app.listen(PORT, () => {
-    //    opn("http://localhost:" + PORT)
+  //opn("http://localhost:" + PORT);
 });
